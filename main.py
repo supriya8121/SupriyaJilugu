88e2412061b0d432957840869e2200ee45f2e411
Supriya Jilugu <45100027+supriya8121@users.noreply.github.com>
2026-05-25 16:45:30 +0530

    API GET request test

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+
+url = "https://jsonplaceholder.typicode.com/posts/1"
+
+response = requests.get(url)
+
+print("Status Code:", response.status_code)
+print("Response Data:")
+print(response.json())
